0acd520114c51ef04810808398b66ef59140d1bb
Adrienne Salkey <aesalkey@gmail.com>
2023-10-26 10:42:25 -0400

    Added pickup script and ui, fixed sprite movement

---
+++ scaredy cat/Assets/Scripts/ChickenPickup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChickenPickup : MonoBehaviour
+{
+    private int chicken = 0;
+    // Start is called before the first frame update
+
+    [SerializeField] private Text chickenText;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+          if(other.gameObject.CompareTag("Chicken"))
+        {
+            Destroy(other.gameObject);
+            chicken++;
+            chickenText.text = "Chickens: " + chicken;
+        }
+    }
+}
